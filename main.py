fc87f238859cca6ef469b232c01a2c2d3704e3d9
Ahmad <muahmad710@gmail.com>
2022-12-15 15:08:19 +0500

    Add a post request

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ response = requests.get('https://httpbin.org/get')
 
 # get status code
 print(response.status_code)
-
 res_json = response.json()
 
 # the origin field contains the IP Address of a user, - delete that if you're pasting a screenshot or something
 print(res_json) 
 
+payload = {
+    'name' : 'Ahmad',
+    'age' : 21,
+}
+
+# Send a post request
+response_post = requests.post('https://httpbin.org/post', data=payload)
+res_json_post = response_post.json()
+print(res_json_post)
