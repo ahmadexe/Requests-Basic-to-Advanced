217edd5657b67cb94429dd88e6fe4fdc76848e61
Ahmad <muahmad710@gmail.com>
2022-12-15 15:22:49 +0500

    Changing user agent

--- main.py
+++ main.py
@@ -18,3 +18,10 @@ payload = {
 response_post = requests.post('https://httpbin.org/post', data=payload)
 res_json_post = response_post.json()
 print(res_json_post)
+
+# Chaing user-agent - using headers (Default user agent is python requests)
+headers = {
+    'User-Agent' : 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36'
+}
+res = requests.get('https://httpbin.org/user-agent', headers=headers)
+print(res.text)
